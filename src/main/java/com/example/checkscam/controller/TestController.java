ef5cd5584444b33d362e70305bf7720bc8626870
An Pham <vnswann@gmail.com>
2025-04-25 00:56:04 +0700

    Base project

---
+++ src/main/java/com/example/checkscam/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.checkscam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String getHelloWrold(){
+        return "Hello World";
+    }
+}
