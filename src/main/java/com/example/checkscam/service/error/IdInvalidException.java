cda680f9f71ef9936286071b5afbdc9ec6ac7b30
An Pham <vnswann@gmail.com>
2025-04-30 01:00:55 +0700

    base format response

---
+++ src/main/java/com/example/checkscam/service/error/IdInvalidException.java
@@ -0,0 +1,8 @@
+package com.example.checkscam.service.error;
+
+public class IdInvalidException extends Exception {
+    // Constructor that accepts a message
+    public IdInvalidException(String message) {
+        super(message);
+    }
+}
