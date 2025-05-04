bdf3b5d89f173dc0453e0eab1744ccad09573624
tientm <tient4m@gmail.com>
2025-05-04 23:08:57 +0700

    giả lập luồng call tra cứu với GPT

--- src/main/java/com/example/checkscam/service/BankScamStatsService.java
+++ src/main/java/com/example/checkscam/service/BankScamStatsService.java
@@ -1,4 +1,7 @@
 package com.example.checkscam.service;
 
+import com.example.checkscam.repository.projection.BankScamStatsInfo;
+
 public interface BankScamStatsService {
+    BankScamStatsInfo getBankScamStatsInfo(String bankAccount);
 }
--- src/main/java/com/example/checkscam/service/impl/BankScamStatsServiceImpl.java
+++ src/main/java/com/example/checkscam/service/impl/BankScamStatsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.checkscam.service.impl;
 
 import com.example.checkscam.repository.BankScamStatsRepository;
+import com.example.checkscam.repository.projection.BankScamStatsInfo;
 import com.example.checkscam.service.BankScamStatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BankScamStatsServiceImpl implements BankScamStatsService {
     private final BankScamStatsRepository repository;
+
+    @Override
+    public BankScamStatsInfo getBankScamStatsInfo(String bankAccount) {
+        return repository.findByBankAccount(bankAccount);
+    }
 }
